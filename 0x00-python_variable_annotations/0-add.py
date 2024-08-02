33ea0bc541e31302beb19a9803e0e4c5408555ac
paxxchal <paschalgabrielacj8@gmail.com>
2024-08-02 09:56:51 +0100

    completed a task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function for adding two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers together.
+
+    :param a: First float number
+    :param b: Second float number
+    :return: The sum of a and b as a float
+    """
+    return a + b
