538b65d41ba757a5d76c21c082a1b85c431068b9
paxxchal <paschalgabrielacj8@gmail.com>
2024-08-05 18:38:37 +0100

    fixed an error

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,37 @@
+#!/usr/bin/env python3
+"""
+This module contains an async routine that spawns wait_random n times.
+"""
+
 import asyncio
 from typing import List
-import importlib
-wait_random = importlib.import_module('0-basic_async_syntax').wait_random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay to be used in wait_random.
+
+    Returns:
+        List[float]: List of delays in ascending order.
+    """
     delays = []
+    tasks = []
 
-    async def spawn_wait_random():
-        delay = await wait_random(max_delay)
+    for _ in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
         delays.append(delay)
-        return delay
 
-    await asyncio.gather(*(spawn_wait_random() for _ in range(n)))
+    return delays
 
-    return sorted(delays)
+
+if __name__ == "__main__":
+    print(asyncio.run(wait_n(5, 5)))
